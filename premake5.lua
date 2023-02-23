27c51fe547df4ef1660e9ff5c41fbcc3574dbcb5
gustavo-tomas <guga.tomas10@gmail.com>
2023-02-23 17:27:27 -0300

    Adicionando arquivos ao premake

--- premake5.lua
+++ premake5.lua
@@ -9,32 +9,23 @@ project "GLFW"
     {
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+        "src/**.c", -- Eu desisto
+        "src/**.h"  -- Sem vergonha nenhuma
     }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+
+        defines
+        {
+            "_GLFW_X11"
+        }
 
     filter "system:windows"
         buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
         staticruntime "On"
-
-        files
-        {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c",
-        }
 
         defines
         {
