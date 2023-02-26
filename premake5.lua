c1665795cbd1f00636cd6bc4e9b616c5e80cf570
gustavo-tomas <guga.tomas10@gmail.com>
2023-02-26 19:03:09 -0300

    Atualizando premake

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -14,8 +15,7 @@ project "GLFW"
     }
 
     filter "system:linux"
-        pic "On"
-        systemversion "latest"
+        pic "on"
 
         defines
         {
@@ -23,9 +23,7 @@ project "GLFW"
         }
 
     filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
         systemversion "latest"
-        staticruntime "On"
 
         defines
         {
@@ -33,5 +31,10 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
